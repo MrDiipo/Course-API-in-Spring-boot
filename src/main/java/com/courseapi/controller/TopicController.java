9a1b10c7a843f766d483ef56f330f387eb056a08
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-08-06 02:31:11 +0100

    Topic model addition and controller enhancement.

--- src/main/java/com/courseapi/controller/TopicController.java
+++ src/main/java/com/courseapi/controller/TopicController.java
@@ -1,13 +1,21 @@
 package com.courseapi.controller;
 
+import com.courseapi.models.Topic;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class TopicController {
 
     @RequestMapping("/topics")
-    public String getAllTopics(){
-        return "All Topics";
+    public List<Topic> getAllTopics(){
+        return Arrays.asList(
+                (new Topic( "Spring", "Spring Framework", "Spring Framework Description")),
+                (new Topic("Java", "Core Java", "Core Java Description")),
+                (new Topic("Javascript", "Fluent JavaScript", "Javascript Description"))
+        );
     }
 }
