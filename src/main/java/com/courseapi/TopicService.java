5ae5b7cdf42bf0f02bd63d6317a03a27e9329ee6
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-08-08 03:52:30 +0100

    Implementing update and delete.

--- src/main/java/com/courseapi/TopicService.java
+++ src/main/java/com/courseapi/TopicService.java
@@ -10,8 +10,8 @@ import java.util.List;
 @Service
 public class TopicService {
 
-    private List<Topic> topics =  new ArrayList<>(Arrays.asList(
-            (new Topic( "Spring", "Spring Framework", "Spring Framework Description")),
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            (new Topic("Spring", "Spring Framework", "Spring Framework Description")),
             (new Topic("Java", "Core Java", "Core Java Description")),
             (new Topic("Javascript", "Fluent JavaScript", "Javascript Description"))
     ));
@@ -20,11 +20,25 @@ public class TopicService {
         return topics;
     }
 
-    public Topic getTopic(String id){
+    public Topic getTopic(String id) {
         return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
     }
 
     public void addTopic(Topic topic) {
         topics.add(topic);
     }
+
+    public void updateTopic(String id, Topic topic) {
+        for (int i = 0; i < topics.size(); i++) {
+            Topic t = topics.get(i);
+            if (t.getId().equals(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id, Topic topic) {
+        topics.removeIf(topic1 -> topic1.getId().equals(id));
+            }
 }
